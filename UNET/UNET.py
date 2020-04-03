e2ab7ab88d3c1ea92fa9d34f9296e3a1595e255d
Conor <conorwhitley2@hotmail.com>
2020-04-03 19:08:01 +0100

    Synching image generator stuff, starting to work on masks

--- UNET/UNET.py
+++ UNET/UNET.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-import tensorflow.keras.layers
+from tensorflow.keras import layers
 from tensorflow.keras.models import Model
 
 
@@ -8,22 +8,19 @@ class UNet:
 
 	def __init__(self, n_classes, input_size):
 
-		self.n_classes = kwargs.get(n_classes)
+		self.n_classes = n_classes
 		self.input_size = input_size
 
 	def make_unet(self):
 
 		input_ = layers.Input(self.input_size)
 
-		c1 = layers.Conv2D(32, (3,3), kernel_initializer = "he_normal", padding = "same", batchnorm = True)(input_)
+		c1 = layers.Conv2D(32, (3,3), kernel_initializer = "he_normal", padding = "same")(input_)
 		p1 = layers.MaxPooling2D((2, 2))(c1)
 		p1 = layers.Dropout(0.1)(p1)
 
 		outputs = layers.Conv2D(1, (1, 1), activation='sigmoid')(c1)
 
 
-		self.model = layers.Model(inputs = [input_], outputs = [outputs])
+		self.model = Model(inputs = [input_], outputs = [outputs])
 		self.model.compile(optimizer = "Adam", loss = "categorical_crossentropy", metrics = ["accuracy"])
-
-
-print("Hello")
--- UNET/UNETprocess.py
+++ UNET/UNETprocess.py
@@ -1,17 +1,46 @@
+import numpy as np
+import pandas as pd
+
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 
+
 def get_reduced(im, n_components = 10):
-    
-    shape = im.shape
-    tab = im.reshape(-1, shape[-1])
-    
-    pipe = Pipeline([
-        #("Scaler", StandardScaler()),
-        ("PCA", PCA(n_components = n_components))
-    ])
-    
-    tab = pipe.fit_transform(tab)
-    
-    return tab.reshape(shape[0], shape[1], -1)
+	
+	shape = im.shape
+	tab = im.reshape(-1, shape[-1])
+	
+	pipe = Pipeline([
+		("Scaler", StandardScaler()),
+		("PCA", PCA(n_components = n_components))
+	])
+	
+	tab = pipe.fit_transform(tab)
+	
+	return tab.reshape(shape[0], shape[1], -1)
+
+
+def image_import(file, transformer, start = 1000, end = 1800, paraffin = (1340, 1490)):
+
+	image = file.load()
+	wn = np.array(image.metadata["wn"], dtype = float)
+	
+	starti, endi, psi, pei = [np.argmin(np.abs(i - wn)) for i in [start, end, paraffin[0], paraffin[1]]]
+	
+	
+	tab = pd.DataFrame(image.reshape(-1, image.shape[-1]), columns = wn)
+	
+	# Only use wavenumbers within specified range
+	tab = tab.iloc[:,starti:endi]
+
+	if not paraffin == False:
+		# Drop paraffin
+		tab = tab.drop(wn[psi:pei], axis = 1)
+
+
+
+	tab = transformer.fit_transform(tab.T).T
+
+
+	return tab.reshape(image.shape[:-1]+(-1,))
